6dedfa795c6be2468b90c85b4e1755ba1f47e579
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-12 09:35:52 -0600

    add project files

---
+++ src/test/java/com/curso/microservicios/spring/app/zuul/CursoMicroserviciosSpringMicroservicioZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroserviciosSpringMicroservicioZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
